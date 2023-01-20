f60d2bc9bd88123961bb4b6fd8dfb08d60912a58
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-01-20 14:54:23 +0200

    3 push

---
+++ 03. Table Ralations - Exercise/SQLQuery2.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE [OnlineStoreDb]
+
+USE [OnlineStoreDb]
+
+CREATE TABLE [Cities]
+(
+	[CityID] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [ItemTypes]
+(
+	[ItemTypeID] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Items]
+(
+	[ItemID] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[ItemTypeID] INT FOREIGN KEY REFERENCES [ItemTypes]([ItemTypeID])
+)
+
+CREATE TABLE [Customers]
+(
+	[CustomerID] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Birthday] DATETIME2 NOT NULL,
+	[CityID] INT FOREIGN KEY REFERENCES [Cities]([CityID])
+)
+
+
+create table [Orders]
+(
+    OrderID INT PRIMARY KEY IDENTITY,
+    CustomerID INT FOREIGN KEY REFERENCES [Customers](CustomerID)
+)
+
+create table [OrderItems]
+(
+    OrderID INT FOREIGN KEY REFERENCES[Orders](OrderID),
+    ItemID INT FOREIGN KEY REFERENCES [Items](ItemID)
+    PRIMARY KEY(OrderID,ItemID)
+)
